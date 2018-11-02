2ad0786a85ecf13e0e8898f98365dd9ec832bd80
Michael Leonffu <leonffu.michael@gmail.com>
2018-11-02 13:29:34 -0700

    Update configTEMPLATE.js
    To run this program; should replace the configTEMPLATE.js with config.js and actual information

---
+++ configTEMPLATE.js
@@ -0,0 +1,12 @@
+//Config for Anna Li bot
+
+//REMOVE TEMPLATE FROM THE FILE NAME, SHOULD BE "condig.js"
+
+module.exports = {
+	apiKey:{
+		discord: 'super secert Anna Li discord Key ˚∆˚',
+		gif: 'super secert Anna Li gif Key ˚∆˚',
+		tumblr: 'super secert Anna Li tumblr Key ˚∆˚'
+	},
+	databaseURL: 'mongodb://annali:42'
+}
