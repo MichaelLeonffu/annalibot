fce37cfa02d4562d3d4e3168fdaed4498b4014c3
Michael Leonffu <leonffu.michael@gmail.com>
2020-08-05 13:52:06 -0700

    Added Admin, and reload command

--- annapythonlibot/annapythonli.py
+++ annapythonlibot/annapythonli.py
@@ -5,6 +5,8 @@ import config.config as config
 # This is the discord
 import discord
 from discord.ext import commands
+# Timeit
+import timeit
 
 
 # The bot
@@ -29,6 +31,30 @@ if __name__ == '__main__':
 @bot.event
 async def on_ready():
 	print(f'{bot.user} has connected to Discord!')
+
+# Admin check
+def is_admin(ctx):
+	return ctx.message.author.id == config.ADMIN_ID
+
+# Reloading exentions (cogs and listeners)
+@bot.command(
+	name="reload",
+	hidden=True
+)
+@commands.check(is_admin)
+async def _reload(ctx):
+	confirmation = await ctx.send("Reloading")
+
+	time = timeit.timeit(lambda: [bot.reload_extension(cog) for cog in cogs], number=1)
+
+	await confirmation.edit(content="Done! ({:.4f}s)".format(time))
+
+@_reload.error
+async def _reload_error(self, ctx, error):
+	if isinstance(error, commands.CheckFailure):
+		await ctx.send("B-Baka!!! ><")
+	else:
+		print(error)
 
 
 # Connect the client to discord
