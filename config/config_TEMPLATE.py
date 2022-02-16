fd619b769754b1cef1aead47bbfa65b8fff46c5a
Michael Leonffu <leonffu.michael@gmail.com>
2022-02-16 05:31:25 -0800

    Added data path to config

--- config/config_TEMPLATE.py
+++ config/config_TEMPLATE.py
@@ -4,28 +4,53 @@ USER_ID = 0
 STATS_TRACKER_CHANNEL_ID = 1010101029393
 ADMIN_ID = 1010101029393
 
+####
+# Application level configuration
+####
+
 DEV_MODE = False
+DATA_PATH = "/path/to/your/data/dir"
 PREFIX = '%'
 PREFIXES = ('a!', 'annali!', 'annali ', 'annali', 'anna li ', 'anna li')
-COGS_LIST = ['cogs.kakTracker', 'cogs.misc', 'cogs.testing', 'cogs.voice', 'cogs.metrics', 'cogs.waifureader', 'cogs.cat', 'cogs.admin']
+COGS_LIST = [
+    'cogs.admin',
+    'cogs.art',
+    'cogs.cat',
+    'cogs.kakTracker',
+    'cogs.metrics',
+    'cogs.misc',
+    'cogs.pfp_tracker',
+    'cogs.seasonal',
+    'cogs.testing',
+    'cogs.voice',
+    'cogs.waifureader',
+]
+
+####
+# Cog Settings
+####
+
+# pfp_tracker
+PFP_DATA_PATH = "pfp_tracker"
+
 
 EMOJI = [
-	":purple_heart:",
-	":brown_heart:",
-	":blue_heart:",
-	":green_heart:",
-	":yellow_heart:",
-	":orange_heart:",
-	":heart:",
-	":white_heart:"
+    ":purple_heart:",
+    ":brown_heart:",
+    ":blue_heart:",
+    ":green_heart:",
+    ":yellow_heart:",
+    ":orange_heart:",
+    ":heart:",
+    ":white_heart:"
 ]
 EMOJI_SEASONAL = [
-	":purple_heart:",
-	":brown_heart:",
-	":blue_heart:",
-	":green_heart:",
-	":yellow_heart:",
-	":orange_heart:",
-	":heart:",
-	":white_heart:"
+    ":purple_heart:",
+    ":brown_heart:",
+    ":blue_heart:",
+    ":green_heart:",
+    ":yellow_heart:",
+    ":orange_heart:",
+    ":heart:",
+    ":white_heart:"
 ]
